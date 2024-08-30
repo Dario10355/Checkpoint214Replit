fe32790ed35c5a20b50e5ac8e7e1897e3694bb32
Dario10355 <dario10355@gmail.com>
2024-08-30 04:06:04 +0000

    Initial Code for Checkpoint 2.14

--- .ccls-cache/@home@runner@Checkpoint21-4Replit/main.cpp
+++ .ccls-cache/@home@runner@Checkpoint21-4Replit/main.cpp
@@ -1,3 +1,28 @@
-#include <iostream>
+/* Program File Name: Checkpoinnt2.14
+   Programmer: Darius Mason
+   Date: August 2024
+   Requierments:
+   Write a program that has the following character      
+   variables: first, middle, and last. Store your initals
+   in these variables the display them on the screen.
+*/
 
-int main() { std::cout << "Hello World!\n"; }
+#include<iostream>
+
+
+
+
+int main()
+{
+   char firstInitial;
+   char middleInitial;
+   char lastInitial;
+   std::cout << "Please enter the initial of your name:";
+   std::cin >> firstInitial;
+   std::cout << "Please enter the initial of your middle name:";
+   std::cin >> middleInitial;
+   std::cout << "Please enter the initial of your last name:";
+   std::cin >> lastInitial;
+   std::cout << "Your initials are: " << firstInitial << middleInitial << lastInitial;
+
+}
