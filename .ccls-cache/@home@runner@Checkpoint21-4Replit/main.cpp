953a839facd0b572453ddac7fa0bba7783925461
Dario10355 <dario10355@gmail.com>
2024-08-30 04:24:20 +0000

    Added code to accept the users first name.

--- .ccls-cache/@home@runner@Checkpoint21-4Replit/main.cpp
+++ .ccls-cache/@home@runner@Checkpoint21-4Replit/main.cpp
@@ -8,7 +8,7 @@
 */
 
 #include<iostream>
-
+#include <string>
 
 
 
@@ -17,12 +17,19 @@ int main()
    char firstInitial;
    char middleInitial;
    char lastInitial;
+   std::string firstName;
+
    std::cout << "Please enter the initial of your name:";
    std::cin >> firstInitial;
    std::cout << "Please enter the initial of your middle name:";
    std::cin >> middleInitial;
    std::cout << "Please enter the initial of your last name:";
    std::cin >> lastInitial;
-   std::cout << "Your initials are: " << firstInitial << middleInitial << lastInitial;
+   std::cout << "Your initials are: " << firstInitial <<middleInitial << lastInitial;
+   std::cout << "Please enter your first name:";
+   std::cin>>firstName;
+   std::cout <<"Welcome "<<firstName;
+
+   
 
 }
